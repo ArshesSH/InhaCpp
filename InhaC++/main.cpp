9f9a06c212edc0f145cc0b89b3e6681a345412c2
ArshesSH <105101004ksh@gmail.com>
2022-05-30 18:09:03 +0900

    Add Timer

--- InhaC++/main.cpp
+++ InhaC++/main.cpp
@@ -1,12 +1,16 @@
+#include <iostream>
 #include "Chapter9.h"
+#include "Timer.h"
 
 int main()
 {
 	while (true)
 	{
+		Timer timer;
 		Chapter9::Question4();
+		float fTime = timer.GetTime();
+		std::cout << "ÃÑ °É¸° ½Ã°£ = " << fTime << std::endl;
 	}
-
 
 	return 0;
 }
